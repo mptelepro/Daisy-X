b2c47399aa6589ce6abdb6eeb8bd2716441f8dda
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-03-20 12:43:09 +0530

    Update sample_config.py

--- DaisyX/sample_config.py
+++ DaisyX/sample_config.py
@@ -14,17 +14,17 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
+    API_ID = 1749924  # integer value, dont use ""
+    API_HASH = "87e4a056bb574f7939ac283b9a9187ab"
+    TOKEN = "1647542126:AAEsyG7tkiKbDLUcJUmtXHUuCZNSSxnDFZw"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 984441749  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Munnipopz"
+    SUPPORT_CHAT = "mpazaanbot"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001367221700
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1001367221700
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
